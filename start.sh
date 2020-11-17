6c2158bd297ed42f819d16dc3cf460fec0f74f00
maninmiddle <69719345+maninmiddle@users.noreply.github.com>
2020-11-18 01:23:45 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 banner() {
 
-printf "\e[1;31m_  _   _______ ____  _   _ _____ _       ____   _    _   _ ___ ____ \e[0m\n"
-printf "\e[1;31m_ | |/ / ____|  _ \| \ | | ____| |     |  _ \ / \  | \ | |_ _/ ___| \e[0m\n" 
-printf "\e[1;31m_ | ' /|  _| | |_) |  \| |  _| | |     | |_) / _ \ |  \| || | |     \e[0m\n"
-printf "\e[1;31m_ | . \| |___|  _ <| |\  | |___| |___  |  __/ ___ \| |\  || | |___  \e[0m\n"
-printf "\e[1;31m_ |_|\_\_____|_| \_\_| \_|_____|_____| |_| /_/   \_\_| \_|___\____| \e[0m\n"
+printf "\e[1;31m  _   _______ ____  _   _ _____ _       ____   _    _   _ ___ ____ \e[0m\n"
+printf "\e[1;31m | |/ / ____|  _ \| \ | | ____| |     |  _ \ / \  | \ | |_ _/ ___| \e[0m\n" 
+printf "\e[1;31m | ' /|  _| | |_) |  \| |  _| | |     | |_) / _ \ |  \| || | |     \e[0m\n"
+printf "\e[1;31m | . \| |___|  _ <| |\  | |___| |___  |  __/ ___ \| |\  || | |___  \e[0m\n"
+printf "\e[1;31m |_|\_\_____|_| \_\_| \_|_____|_____| |_| /_/   \_\_| \_|___\____| \e[0m\n"
 printf "\n                                                                          \e[0m\n"
 
 }
@@ -19,14 +19,25 @@ read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m\en' op
 
 if [[ $option == 1 ]]; then
 clear
-pkg install python php wget openssh openssl nano -y
+printf "\e[1;93m Installing other Defualt packages... Please be patient \e[0m\n"
+sleep 1
+pkg install python php wget openssh openssl nano git -y
 clear 
 elif [[ $option == 2 ]]; then
-server="facebook"
-start
+clear
+pkg install tsu -y
+pkg install unstable-repo root-repo -y
+clear
+printf "\e[1;93m Installing other Root packages... Please be patient \e[0m\n"
+sleep 1
+clear
+pkg install aircrack-ng bettercap termshark -y
 elif [[ $option == 3 ]]; then
-server="snapchat"
-start
+clear 
+pkg install unstable-repo -y
+printf "\e[1;93m Installing other Special packages... It may take a long time. Please be patient \e[0m\n"
+sleep 1
+pkg install metasploit sqlmap rapidjson -y 
 else
 printf "\e[1;93m [!] Invalid option!\e[0m\n"
 menu
