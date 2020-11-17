b80d464ef0c2953f0446ebd51550096a28d365bc
maninmiddle <69719345+maninmiddle@users.noreply.github.com>
2020-11-18 01:29:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ printf "\e[1;31m | ' /|  _| | |_) |  \| |  _| | |     | |_) / _ \ |  \| || | |  
 printf "\e[1;31m | . \| |___|  _ <| |\  | |___| |___  |  __/ ___ \| |\  || | |___  \e[0m\n"
 printf "\e[1;31m |_|\_\_____|_| \_\_| \_|_____|_____| |_| /_/   \_\_| \_|___\____| \e[0m\n"
 printf "\n                                                                          \e[0m\n"
+printf "\e[1;93m       ●\e[0m\e[1;77m coded by: @maninmiddle \e[0m\e[1;93m●\e[0m\n"
 
 }
 
@@ -19,7 +20,7 @@ read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m\en' op
 
 if [[ $option == 1 ]]; then
 clear
-printf "\e[1;93m Installing other Defualt packages... Please be patient \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m!\e[0m\e[1;92m]\e[0m\e\e[1;93m Installing other Defualt packages... Please be patient \e[0m\n"
 sleep 2
 pkg install python php wget openssh openssl nano git -y
 clear 
@@ -35,7 +36,7 @@ pkg install aircrack-ng bettercap termshark -y
 elif [[ $option == 3 ]]; then
 clear 
 pkg install unstable-repo -y
-printf "\e[1;93m Installing other Special packages... It may take a long time. Please be patient \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m!\e[0m\e[1;92m]\e[0m\e\e[1;93m Installing other Special packages... Please be patient \e[0m\n"
 sleep 2
 pkg install metasploit sqlmap rapidjson -y 
 else
